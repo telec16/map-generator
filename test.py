22c71855e1fd2d607ae44e888d7b7fe11c8a1a5c
telec16 <diode16@free.fr>
2018-02-07 15:06:46 +0100

    "commit amphi"

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 print "coucou"
 
-retrieve config ('\\')
-split "\n\n"
-split at first \n\w:
-create objects with class 'Blueprint'
+with open("buildings.cfg", "r") as file:
+  raw = file.read()
+  blueprint = raw.split("\n\n")
+  cfgRe = re.pattern("\\(\w):(\d*)")
+  configs = cfgRe.search(blueprint[0])
+  blueprint.unset(blueprint[0])
 
-select random bluid
-create new object from object 'Blueprint' with random values with class 'Build'
-put randomly on map (and check possibility)
+#retrieve config ('\\')
+#split "\n\n"
+#split at first \n\w:
+#create objects with class 'Blueprint'
 
-stop if maxtry or maxfill
+#select random bluid
+#create new object from object 'Blueprint' with random values with class 'Build'
+#put randomly on map (and check possibility)
+
+#stop if maxtry or maxfill
 
