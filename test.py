2592b96988a1044a91de103c667064c16e474311
telec16 <diode16@free.fr>
2018-02-07 13:52:11 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+print "coucou"
+
+retrieve config ('\\')
+split "\n\n"
+split at first \n\w:
+create objects with class 'Blueprint'
+
+select random bluid
+create new object from object 'Blueprint' with random values with class 'Build'
+put randomly on map (and check possibility)
+
+stop if maxtry or maxfill
+
